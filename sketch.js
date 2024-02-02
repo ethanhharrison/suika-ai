272d626e560a0d97e937b871aabd2fbd947b9105
E Harrison <ehharrison13@berkeley.edu>
2024-02-02 02:31:27 -0800

    refactoring

--- sketch.js
+++ sketch.js
@@ -5,30 +5,30 @@ var Engine = Matter.Engine,
   Composite = Matter.Composite,
   Collision = Matter.Collision,
   Events = Matter.Events;
+// matter.js variables
+var composite;
+var engine;
+var world;
+// matter bodies
+var barriers;
+var fruits;
+var currFruit;
+var ground;
+var lWall;
+var rWall;
+// game variables
+var gameOver;
+var totalPoints;
+// game startup settings
+var boxWidth;
+var boxHeight;
+var boxThickness;
 
 const startingFruits = Array("cherry", "strawberry", "grape", "dekopon", "orange")
 Array.prototype.random = function () {
   var index = Math.floor((Math.random()*this.length));
   return this[index];
 }
-
-var composite;
-var engine;
-var world;
-
-var barriers = {};
-var fruits = {};
-var currFruit;
-var ground;
-var leftWall;
-var rightWall;
-
-var gameOver;
-var totalPoints;
-
-var boxWidth;
-var boxHeight;
-var boxThickness;
 
 function setup() {
   createCanvas(600, 700);
@@ -39,40 +39,20 @@ function setup() {
   boxHeight = height * 0.5;
   boxThickness = 30;
 
-  var options = {
+  var engineOptions = {
     gravity: {scale: 0.0005 },
     positionIterations: 50,
     velocityIterations: 50,
-    timing: {timeScale: 1.5}
+    timing: {timeScale: 1}
   }
-  engine = Engine.create(options);
+  engine = Engine.create(engineOptions);
   world = engine.world;
-  Runner.run(engine);
+  Runner.run(engine); 
 
-  Events.on(engine, "collisionStart", function (event) {
-    var pairs = event.pairs;
-    for (var i = 0; i < pairs.length; i++) {
-      bodyA = pairs[i].bodyA;
-      bodyB = pairs[i].bodyB;
-      if (bodyA.circleRadius == bodyB.circleRadius) {
-        var fruitA = fruits[bodyA.id];
-        var fruitB = fruits[bodyB.id];
+  checkCollisions();
 
-        var x = (fruitA.x + fruitB.x) / 2;
-        var y = (fruitA.y + fruitB.y) / 2;
-        var type = fruitHeirarchy[fruitA.idx + 1];
-
-        var combinedFruit = new Fruit(x, y, type);
-        combinedFruit.combined();
-        fruits[combinedFruit.body.id] = combinedFruit;
-        totalPoints += combinedFruit.points;
-
-        delete fruits[bodyA.id];
-        delete fruits[bodyB.id];
-        Composite.remove(world, [bodyA, bodyB]);
-      }
-    }
-  })  
+  barriers = {};
+  fruits = {};
 
   ground = new Barrier(width / 2, height - boxThickness / 2, boxWidth, boxThickness);
   lWall = new Barrier((width - boxWidth) / 2, height - boxHeight / 2, boxThickness, boxHeight);
@@ -81,6 +61,33 @@ function setup() {
 
   gameOver = false;
   totalPoints = 0;
+}
+
+function checkCollisions() {
+  Events.on(engine, "collisionStart", function (event) {
+    var pairs = event.pairs;
+    for (var i = 0; i < pairs.length; i++) {
+      bodyA = pairs[i].bodyA;
+      bodyB = pairs[i].bodyB;
+      if (bodyA.circleRadius == bodyB.circleRadius) {
+        var fruitA = fruits[bodyA.id];
+        var fruitB = fruits[bodyB.id];
+  
+        var x = (fruitA.x + fruitB.x) / 2;
+        var y = (fruitA.y + fruitB.y) / 2;
+        var type = fruitHeirarchy[fruitA.idx + 1];
+  
+        var combinedFruit = new Fruit(x, y, type);
+        combinedFruit.combined();
+        fruits[combinedFruit.body.id] = combinedFruit;
+        totalPoints += combinedFruit.points;
+  
+        delete fruits[bodyA.id];
+        delete fruits[bodyB.id];
+        Composite.remove(world, [bodyA, bodyB]);
+      }
+    }
+  });
 }
 
 function mousePressed() {
