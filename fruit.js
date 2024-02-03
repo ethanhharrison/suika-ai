a615eaff5f9c3c75498bdbb0425bdb65ee100115
E Harrison <ehharrison13@berkeley.edu>
2024-02-02 17:52:55 -0800

    sizing fruit properly

--- fruit.js
+++ fruit.js
@@ -1,17 +1,3 @@
-const fruitTypes = {
-  cherry:     { idx: 0, rgb: [255, 0, 0], r: 15, points: 1 },
-  strawberry: { idx: 1, rgb: [255, 105, 180], r: 25, points: 3 },
-  grape:      { idx: 2, rgb: [138, 43, 226], r: 35, points: 6 },
-  dekopon:    { idx: 3, rgb: [255, 215, 0], r: 45, points: 10 },
-  orange:     { idx: 4, rgb: [255, 140, 0], r: 55, points: 15 },
-  apple:      { idx: 5, rgb: [255, 0, 0], r: 65, points: 21 },
-  pear:       { idx: 6, rgb: [255, 87, 51], r: 75, points: 28 },
-  peach:      { idx: 7, rgb: [255, 229, 180], r: 85, points: 36 },
-  pineapple:  { idx: 8, rgb: [255, 255, 101], r: 95, points: 45 },
-  honeydew:   { idx: 9, rgb: [240, 255, 240], r: 105, points: 55 },
-  watermelon: { idx: 10, rgb: [0, 121, 68], r: 115, points: 66 },
-};
-
 const fruitHeirarchy = Array("cherry", "strawberry", "grape", 
                              "dekopon", "orange", "apple", 
                              "pear", "peach", "pineapple", 
--- sketch.js
+++ sketch.js
@@ -19,6 +19,7 @@ var rWall;
 // game variables
 var gameOver;
 var totalPoints;
+var fruitTypes;
 // game startup settings
 var boxWidth;
 var boxHeight;
@@ -31,13 +32,27 @@ Array.prototype.random = function () {
 }
 
 function setup() {
-  createCanvas(600, 700);
+  createCanvas(800, 900);
   textAlign(CENTER);    
   rectMode(CENTER);
 
-  boxWidth = width * 0.6;
-  boxHeight = height * 0.5;
+  boxWidth = width * 0.7;
+  boxHeight = boxWidth * 8/7;
   boxThickness = 30;
+
+  fruitTypes = {
+    cherry:     { idx: 0, rgb: [255, 0, 0], r: boxWidth * 50/1400, points: 1 },
+    strawberry: { idx: 1, rgb: [255, 105, 180], r: boxWidth * 60/1400, points: 3 },
+    grape:      { idx: 2, rgb: [138, 43, 226], r: boxWidth * 100/1400, points: 6 },
+    dekopon:    { idx: 3, rgb: [255, 215, 0], r: boxWidth * 115/1400, points: 10 },
+    orange:     { idx: 4, rgb: [255, 140, 0], r: boxWidth * 140/1400, points: 15 },
+    apple:      { idx: 5, rgb: [255, 0, 0], r: boxWidth * 175/1400, points: 21 },
+    pear:       { idx: 6, rgb: [255, 87, 51], r: boxWidth * 200/1400, points: 28 },
+    peach:      { idx: 7, rgb: [255, 229, 180], r: boxWidth * 250/1400, points: 36 },
+    pineapple:  { idx: 8, rgb: [255, 255, 101], r: boxWidth * 300/1400, points: 45 },
+    honeydew:   { idx: 9, rgb: [240, 255, 240], r: boxWidth * 350/1400, points: 55 },
+    watermelon: { idx: 10, rgb: [0, 121, 68], r: boxWidth * 400/1400, points: 66 },
+  };
 
   var engineOptions = {
     gravity: {scale: 0.0005 },
@@ -57,7 +72,7 @@ function setup() {
   ground = new Barrier(width / 2, height - boxThickness / 2, boxWidth, boxThickness);
   lWall = new Barrier((width - boxWidth) / 2, height - boxHeight / 2, boxThickness, boxHeight);
   rWall = new Barrier((width + boxWidth) / 2, height - boxHeight / 2, boxThickness, boxHeight);
-  currFruit = new Fruit(0, height - 5 * boxHeight / 4, startingFruits.random());
+  currFruit = new Fruit(0, height - boxHeight * 900 / 800, startingFruits.random());
 
   gameOver = false;
   totalPoints = 0;
@@ -114,7 +129,7 @@ function reset() {
 function dropFruit() {
   currFruit.drop();
   fruits[currFruit.body.id] = currFruit;
-  currFruit = new Fruit(0, height - 5 * boxHeight / 4, startingFruits.random());
+  currFruit = new Fruit(0, height - boxHeight * 900 / 800, startingFruits.random());
   return currFruit;
 }
 
